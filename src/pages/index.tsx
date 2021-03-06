763feb4005d5758a7a927c978acacdc63af607cb
monichre <liamhellis@gmail.com>
2021-03-06 16:46:35 -0500

    removed dead code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Layout } from "../components/layout";
-import { SEO } from "../components/seo";
 
 import Dashboard from "../components/Dashboard";
 
 const App = () => (
   <Layout>
-    <SEO />
     <Dashboard />
   </Layout>
 );
